0249aeac4ed33487976fc71bca24c662a35b155c
nyhhl <nyhhl@163.com>
2019-03-22 22:51:27 +0800

    1

--- producer_conductor.c
+++ producer_conductor.c
@@ -1,1 +1,23 @@
 #include<unistd.c>
+void producer()
+{
+	while(1)
+	wait();
+
+}
+void conductor()
+{
+}
+int main()
+{
+	for(int i=0;i<10;i++)
+		if(fork()<=0)
+			break;
+		else
+		{
+			producer();
+		}
+
+
+
+}
